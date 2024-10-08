e1eb9d50b7b9a8797be08430b4f5aa2c880bda55
Sean <moriorpandemic@gmail.com>
2024-10-08 22:14:05 +0500

    setup all images for project

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,7 +3,7 @@ import MyHello from '@/components/MyHello.vue'
 </script>
 
 <template>
-  <MyHello />
+  <div class=""></div>
 </template>
 
 <style scoped></style>
--- front-end/src/components/MyHello.vue
+++ front-end/src/components/MyHello.vue
@@ -1,5 +1,7 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <h1>V01GH7 Blog</h1>
+</template>
 
 <style scoped></style>
